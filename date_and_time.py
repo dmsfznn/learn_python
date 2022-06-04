6123ceeae87c2532a425551d3e1ba30ffbbc641c
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-04 08:19:29 +0700

    Program_Date_And_Time_Now

---
+++ date_and_time.py
@@ -0,0 +1,8 @@
+#DATE AND TIME
+# dates are easily constructed and formatted
+from datetime import date
+now = date.today()
+now
+now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B.")
+
+# Selesai1.2
