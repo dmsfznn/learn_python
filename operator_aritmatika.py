8387b67cee39da51ae131ea95879e92b8d69c3fc
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-04 08:02:37 +0700

    Program_Operator_Aritmatika

---
+++ operator_aritmatika.py
@@ -0,0 +1,31 @@
+# PROGRAM OPERATOR ARITMATIKA	
+a = int(input("Inputkan nilai a: "))
+b = int(input("Inputkan nilai b: "))
+
+# Menggunakan operator penjumlahan
+c = a + b
+print(a,"+",b,"=", +c)
+
+# Menggunakan operator pengurangan
+c = a - b
+print(a,"-",b,"=", +c)
+
+# Menggunakan operator perkalian
+c = a * b
+print(a,"x",b,"=", +c)
+
+# Menggunakan operator pembagian
+c = a / b
+print(a,"/",b,"=", +c)
+
+# Menggunakan sisa bagi
+c = a % b
+print(a,"%",b,"=", +c)
+
+# Menggunakan operator pangkat
+c = a ** b
+print(a,"^",b,"=", +c)
+
+#Selesai 1.1
+
+
