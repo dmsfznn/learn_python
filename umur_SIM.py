9c8ce0067aa43b880ea95e22c3d32251882ec202
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-04 12:11:25 +0700

    Program_Struktur_Kondisi_Sederhana

---
+++ umur_SIM.py
@@ -0,0 +1,12 @@
+# STRUKTUR KONDISI
+# cek_umur.py
+umur = int(input("Berapa umur kamu: "))
+umur1 = int(input("Berapa umur maksimal boleh ambil SIM: "))
+if umur >= umur1:
+    print("Kamu boleh membuat SIM")
+else:
+    print("Kamu belum boleh membuat SIM")
+
+
+
+# Selesai1.3
