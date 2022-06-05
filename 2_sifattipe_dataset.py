e819ad8ba0e287810c453692d315ffb03616a57c
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:19:07 +0700

    2_Program_SifatdanTipe_Dataset
    4.94

---
+++ 2_sifattipe_dataset.py
@@ -0,0 +1,11 @@
+# Program sifat tipe data set
+#Set 2.
+
+foo  =  {"Belajar",  "Python",  "di",  "Duniailkom",  "di"} 
+bar  =  {100,  200,  300,  400,  200,  300}
+
+print(foo) 
+print(bar)
+
+
+# Selesai 4.94
