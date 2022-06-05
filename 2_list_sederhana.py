d2d7353a6046983092fd93f448edee3869e83a7b
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 01:09:58 +0700

    2_Program_List_Sederhana
    4.6

---
+++ 2_list_sederhana.py
@@ -0,0 +1,11 @@
+# Program List 
+#2.
+foo  =  ["Python",  200,  6.77,  True,  'Duniailkom',  99j] 
+print(foo[0])
+print(foo[1])
+print(foo[2])
+print(foo[3])
+print(foo[4])
+print(foo[5])
+
+# Selesai 4.6
