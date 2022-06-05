41c42d02556ba62e5797f079a383709be94f4989
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:31:58 +0700

    3_Program_mengakses_dictionary.py
    4.99

---
+++ 3_mengakses_dictionary.py
@@ -0,0 +1,9 @@
+# Program mengakses dictonary
+#Dictionary 3.
+
+foo  =  {  "kegiatan":  "Belajar  Python", "website":  "Duniailkom", "hasil":  "Yakin  Bisa  !"  }
+
+print(foo["website"])
+
+
+# Selesai 4.99
