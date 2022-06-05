fa71013838b35e5d0ece0959fb92de7e394993a9
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:22:17 +0700

    3_Program_Mengakses_Dataset_Menggunakan_Index
    4.95

---
+++ 3_akses_set_dengan_index.py
@@ -0,0 +1,7 @@
+# Program mengakses anggota set menggunakan index
+#Set 3.
+foo  =  {"Belajar",  "Python",  "di",  "Diniailkom",  "di"} 
+print(foo[1])
+
+# Selesai 4.95
+# Pembuktian kalau set tidak bisa di akses dengan index
