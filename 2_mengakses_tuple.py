4f70de448ebb393239ceac1339752ea728528733
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:12:48 +0700

    2_Program_Mengakses_Tuple_Sederhana
    4.91

---
+++ 2_mengakses_tuple.py
@@ -0,0 +1,14 @@
+# Program mngakses Tuple 
+#Tuple #2
+foo  =  ["Python",  200,  6.77,  True,  'Duniailkom',  99j] 
+print(foo[0])
+print(foo[1])
+print(foo[2])
+print(foo[2:5])
+print(foo[:3])
+print(foo[3:])
+print(foo[:])
+
+
+
+# Selesai 4.91
