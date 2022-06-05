c56c9b148b5b19a5f49c6bc4ab25c67d361987fb
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 01:31:56 +0700

    1_Program_Membuat_Tuple
    4.9

---
+++ 1_membuat_tuple.py
@@ -0,0 +1,10 @@
+# Program membuat Tuple 
+# #1.
+
+foo  =  ["Belajar",  "Python",  "di",  "Duniailkom"] 
+print(type(foo))
+foo  =  ["Belajar",  "Python",  "di",  "Duniailkom"]
+print(type(foo))
+
+
+# Selesai 4.9
