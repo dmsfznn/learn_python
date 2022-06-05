fee8e091217b06b9c96fa0c2731d408239e0143b
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-05 23:30:26 +0700

    Program_Rekursif
    3.1

---
+++ programrekursif.py
@@ -0,0 +1,12 @@
+# Program REKURSIF
+def rekursif(angka):
+	if angka > 0 :
+	     	print (angka)
+		angka = angka – 1
+        		rekursif(angka)
+	else :
+		print(angka)
+masukan = int(input(“masukkan angka : “))
+rekursif(masukan)
+
+# Selesai 3.1
