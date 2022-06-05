af38d54fc22524bc8d99a47e913302e5c5a7a2d5
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 01:28:03 +0700

    4_Program_Menampilkan_Sebagian_List
    4.8

---
+++ 4_menampilkan_sebagian_list.py
@@ -0,0 +1,10 @@
+# Program Menampilkan Sebagian List 
+# #4.
+
+foo  =  ["Python",  200,  6.77,  True,  'Duniailkom',  99j] 
+print(foo[2:5])
+print(foo[:3])
+print(foo[3:])
+print(foo[:])
+
+# Selesai 4.8
