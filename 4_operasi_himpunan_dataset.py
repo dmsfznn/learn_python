fb4652cddc62096cb9def8b18440cefbb4304bde
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:24:04 +0700

    4_Program_operasi_himpunan_dataset.py
    4.96

---
+++ 4_operasi_himpunan_dataset.py
@@ -0,0 +1,14 @@
+# Program operasi himpunan data set
+#Set 4.
+
+foo  =  {1,  2,  3,  4,  5}
+bar  =  {3,  4,  5,  6,  7}
+
+#Union
+print (foo | bar)
+
+#Intersection 
+print  (foo  &  bar)
+
+
+# Selesai 4.96
