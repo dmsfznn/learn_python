30bdb4fd28fc5737d21be1f810ab19289f729bdf
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 01:08:01 +0700

    1_Program_List_Sederhana
    4.5

---
+++ 1_list_sederhana.py
@@ -0,0 +1,11 @@
+# Program List 
+# #1.
+foo  =  ["Belajar",  "Python",  "di",  "Duniailkom"] 
+bar  =  [100,  200,  300,  400]
+baz  =  ["Python",  200,  6.99,  True]
+
+print(type(foo)) 
+print(type(bar))
+print(type(baz))
+
+# Selesai 4.5
