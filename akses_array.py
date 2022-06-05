8751b57fe68c4873df8155ac5840de7e843e042f
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-05 23:59:19 +0700

    Program_Mengakses_Elemen_Pada_Array
    4.2

---
+++ akses_array.py
@@ -0,0 +1,22 @@
+#  Importing  array  module import array as arr
+import array as arr
+
+# Array with type
+a  =  arr.array('i',  [1,  2,  3,  4,  5,  6])
+
+#  Accessing  element  of  array print("Acces element is :  ", a[0])
+print("Acces element is :  ", a[0])
+
+#  Accessing  element  of  array print("Acces element is :  ", a[3])
+print("Acces element is :  ", a[3])
+
+#  Array  with  float  type
+b  =  arr.array('d',  [2.5,  3.2,  3.3])
+
+#  Accessing  element  of  array print("Acces element is :  ", b[1])
+print("Acces element is :  ", b[1])
+
+#  Accessing  element  of  array print("Acces element is :  ", b[2])
+print("Acces element is :  ", b[2])
+
+# Selesai 4.2
