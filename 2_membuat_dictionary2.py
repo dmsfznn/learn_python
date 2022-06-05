3125faa253e3ee03765e77c8787603cf5007803d
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:29:40 +0700

    2_Program_membuat_dictionary2
    4.98

---
+++ 2_membuat_dictionary2.py
@@ -0,0 +1,17 @@
+# Program mambuat dictonary
+#Dictionary 2.
+
+foo  =  {   1:  "Belajar",
+            2:  ["Pascal",  
+            "C",  "Python"], 
+            "website":  "Duniailkom", 
+            "menyerah"  :  False,  
+            "target":  2020, 
+            "riwayat_sekolah":  {
+                "SD":  "SDN  3  Hijau  Daun", "SMP":  
+                "SMP  7  Hijau  Lumut", "SMA":  
+                "SMA  8  Hijau  Lumut"}
+}
+print(foo)
+
+# Selesai 4.98
