55719a5870dc78243bdaf8116f6498cc7caf1c4d
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:17:37 +0700

    1_Program_Membuat_Set_Sederhana
    4.93

---
+++ 1_membuat_set.py
@@ -0,0 +1,23 @@
+# Program membuat set
+#Set #1.
+
+foo  =  {"Belajar",  "Python",  "di",  "Duniailkom"} 
+bar  =  {100,  200,  300,  400}
+baz  =  {"Python",  200,  6.99,  True}
+
+
+{'Belajar',  'di',  'Duniailkom',  'Python'}
+{200,  100,  400,  300}
+{200,  True,  6.99,  'Python'}
+
+foo  =  ["Belajar",  "Python",  "di",  "Duniailkom"] 
+print(type(foo))
+
+
+foo  =  ["Belajar",  "Python",  "di",  "Duniailkom"] 
+print(type(foo))
+
+foo  =  ["Belajar",  "Python",  "di",  "Duniailkom"] 
+print(type(foo))
+
+# Selesai 4.93
