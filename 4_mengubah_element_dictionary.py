ed95b559dfb64f28e521d23b12e5f38558165bb3
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:34:06 +0700

    4_Program_mengubah_element_dictionary.py
    4.991

---
+++ 4_mengubah_element_dictionary.py
@@ -0,0 +1,10 @@
+# Program mengubah element dictonary
+#Dictionary 4.
+
+foo  =  {  "kegiatan":  "Belajar  Python", "website":  "Duniailkom", "hasil":  "Yakin  Bisa  !"  }
+
+foo["kegiatan"]  =  "Belajar  Bahasa  Pemrograman" print(foo)
+
+
+
+# Selesai 4.991
