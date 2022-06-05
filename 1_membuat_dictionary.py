ba5392235ca58c3f695ed9e1f268edc12db3a817
Dimas Fauzan Nurhidayat <73967669+dmsfznn@users.noreply.github.com>
2022-06-06 02:25:35 +0700

    1_Program_membuat_dictionary
    4.97

---
+++ 1_membuat_dictionary.py
@@ -0,0 +1,17 @@
+# Program mambuat dictonary
+#Dictionary 1.
+foo  =  {  1:  "Belajar",  2:  "Python",  3:  "di  DuniaIlkom"  }
+bar  =  {"Mengapa":  "Belajar",  "Apa":  "Python",  "Dimana":  "di  DuniaIlkom"  } 
+baz  =  {1:  "Belajar",  "apa":  "Python",  "Dimana":  "di  DuniaIlkom"}
+
+print(type(foo)) 
+print(type(bar)) 
+print(type(baz))
+
+print(foo)
+print(bar) 
+print(baz)
+
+
+
+# Selesai 4.97
